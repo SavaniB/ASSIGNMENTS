1fb495434bb52c84ac79dc9f395e36f2398430ba
SavaniB <147939399+SavaniB@users.noreply.github.com>
2023-10-20 10:08:13 -0700

    Update ARRAY.java

--- ARRAY.java
+++ ARRAY.java
@@ -1,24 +1,23 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class ShuffleArray {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7};
-        shuffleArray(array);
-        System.out.println("Shuffled Array: " + Arrays.toString(array));
+        int[] originalArray = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(originalArray);
+
+        for (int i : originalArray) {
+            System.out.print(i + " ");
+        }
     }
 
     public static void shuffleArray(int[] array) {
-        int n = array.length;
         Random rand = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
 
-        for (int i = n - 1; i > 0; i--) {
-            int j = rand.nextInt(i + 1);
-
-            // Swap array[i] and array[j]
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            int temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
         }
     }
 }
