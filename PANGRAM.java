ab748bcc191ece0af2342f10053b5ba851bba93d
SavaniB <147939399+SavaniB@users.noreply.github.com>
2023-10-20 10:15:59 -0700

    Update PANGRAM.java

--- PANGRAM.java
+++ PANGRAM.java
@@ -1,40 +1,27 @@
 
-       public class PangramChecker {
-    public static void main(String[] args) {
-        String input = "The quick brown fox jumps over the lazy dog"; // Replace with your input string
-        boolean isPangram = isPangram(input);
+     function reverseWordsInSentence(sentence) {
+    // Split the sentence into words using space as a delimiter
+    let words = sentence.split(' ');
+    let reversedWords = [];
 
-        if (isPangram) {
-            System.out.println("The input is a pangram.");
-        } else {
-            System.out.println("The input is not a pangram.");
-        }
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        let reversedWord = reverseWord(word);
+        reversedWords.push(reversedWord);
     }
 
-    public static boolean isPangram(String str) {
-        // Create an array to mark the presence of each letter in the alphabet
-        boolean[] alphabetPresent = new boolean[26];
-
-        // Convert the input string to lowercase to handle uppercase and lowercase letters
-        str = str.toLowerCase();
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-
-            // Check if the character is a lowercase letter
-            if (c >= 'a' && c <= 'z') {
-                int index = c - 'a';
-                alphabetPresent[index] = true;
-            }
-        }
-
-        // Check if all letters from a to z are present in the input
-        for (boolean present : alphabetPresent) {
-            if (!present) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+    return reversedWords.join(' ');
 }
+
+function reverseWord(word) {
+    let reversed = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversed += word[i];
+    }
+    return reversed;
+}
+
+// Example usage:
+let inputSentence = "This is a sunny day";
+let reversedSentence = reverseWordsInSentence(inputSentence);
+console.log(reversedSentence);
