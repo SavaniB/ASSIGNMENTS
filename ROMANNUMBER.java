ea49f97916a948831b690baa8973783f67f8c66f
SavaniB <147939399+SavaniB@users.noreply.github.com>
2023-10-20 10:11:09 -0700

    Update ROMANNUMBER.java

--- ROMANNUMBER.java
+++ ROMANNUMBER.java
@@ -1,37 +1,36 @@
 import java.util.HashMap;
-import java.util.Map;
 
 public class RomanToInteger {
     public static void main(String[] args) {
-        String romanNumeral = "IX"; // Replace with the Roman numeral you want to convert
-        int result = romanToInt(romanNumeral);
-        System.out.println("Integer representation: " + result);
+        String romanNumeral = "IX"; // Replace with your Roman numeral
+
+        int result = romanToInteger(romanNumeral);
+        System.out.println("Integer value: " + result);
     }
 
-    public static int romanToInt(String s) {
-        Map<Character, Integer> romanToInteger = new HashMap<>();
-        romanToInteger.put('I', 1);
-        romanToInteger.put('V', 5);
-        romanToInteger.put('X', 10);
-        romanToInteger.put('L', 50);
-        romanToInteger.put('C', 100);
-        romanToInteger.put('D', 500);
-        romanToInteger.put('M', 1000);
+    public static int romanToInteger(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
 
         int result = 0;
         int prevValue = 0;
 
         for (int i = s.length() - 1; i >= 0; i--) {
-            char currentChar = s.charAt(i);
-            int currentValue = romanToInteger.get(currentChar);
+            int currValue = romanMap.get(s.charAt(i));
 
-            if (currentValue < prevValue) {
-                result -= currentValue;
+            if (currValue >= prevValue) {
+                result += currValue;
             } else {
-                result += currentValue;
+                result -= currValue;
             }
 
-            prevValue = currentValue;
+            prevValue = currValue;
         }
 
         return result;
